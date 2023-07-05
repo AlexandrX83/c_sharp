7b457c441bbc7454848960fd5a5d84dc4be02404
Александр <sun-vallay@yandex.ru>
2023-07-05 23:52:20 +0300

    Добавили вторую папку

---
+++ task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Домашнее задание №1.
+//Задача №2. Написать программу, которая на вход принимает два числа и выдаёт,какое больше,какое меньше.
+Console.WriteLine("Введите первое число (A) -->");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число (B) -->");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b) 
+   {
+    Console.WriteLine($" {a} меньше {b}");
+   }
+  else if (a>b)
+     {
+      Console.WriteLine($" {b} меньше {a}");
+     }   
+   else 
+       {
+        Console.WriteLine($" {a} равно {b}");
+       }
+    
+
+   
