81479b26a80e7b404772fc313519cd4191e78c85
Александр <sun-vallay@yandex.ru>
2023-07-06 19:41:31 +0300

    Making works on seminar 06 07 23

---
+++ task35/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 35. Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своем решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+int fillNumberArray (int [] array, int start, int end)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++ )
+    {
+        if (array[i] >= start && array[i] <= end)
+        {
+            count = count + 1;
+        }
+    }
+    return count; 
+}
+
+int [] getRandomArray(int length, int deviation)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("[");
+    int []result = new int [length];
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(0, deviation + 1);
+    }
+    return result;
+}
+
+int [] myArray = getRandomArray (123, 1000);
+printArray (myArray);
+
+int result = fillNumberArray (myArray, 10, 99);
+Console.WriteLine($"Количество двухзначных чисел в массиве - {result} ");
--- task_02/Program.cs
+++ task_02/Program.cs
@@ -8,4 +8,4 @@ int c = Convert.ToInt32(Console.ReadLine());
 int max =a;
 if (b>max) max = b;
 if (c>max) max = c;
-Console.WriteLine($"Максимальное число {max}");
+Console.WriteLine($"Максимальное число {max}"); 
