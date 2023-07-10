1d78b7dfb32111158e11b0a2796d3ac4320dbdaa
Александр <sun-vallay@yandex.ru>
2023-07-10 17:48:46 +0300

    Добавил комментарии к 38 задаче

---
+++ my_task/Program.cs
@@ -0,0 +1,22 @@
+﻿// 
+double[]  myRndmArr(int Length, double min, double max)
+          {
+            Random rnd = new Random();
+            double[] locArr = new double[Length];
+            return locArr;
+          }
+
+double[] ResArr = myRndmArr(5,10,15);
+for (int i=0; i<5;i++)
+      { if (i<4) 
+         System.Console.Write($"{ResArr[i]} , ");
+         else 
+         System.Console.WriteLine($"{ResArr[i]} . ");
+      }
+
+
+
+
+
+
+
