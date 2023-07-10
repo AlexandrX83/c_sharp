284ab31c2417b3e1380de88182a89e593e96d6c9
Александр <sun-vallay@yandex.ru>
2023-07-10 17:47:17 +0300

    Добавил комментарии к задаче 38

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,18 +1,20 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
-double[] createRandDoubleArr(int size, int min, int max)
+double[] createRandDoubleArr(int size, int min, int max) // метод. Возвращаемый тип данных - массив типа Дабл. 
+                            //Параметры -размер,минимальное значение,максимальное значение
 {
     Random rnd = new Random();
     double[] randArr = new double[size];
 
     for(int i = 0; i < size; i++)
         randArr[i] = (rnd.NextDouble() * (max - min) + min);
-    return randArr;
+    return randArr; // метод createRandDoubleArr возращает заполненный вещ числами массив randArr. При этом при имени переменной 
+                    // типа массив не указывается не тип возращаеммых данных,ни спец символы [], это указанно при объявлении метода   
 }
 
 System.Console.Write("Массив вещественных чисел--> ");
-void printArr(double[] arr)
-{
+void printArr(double[] arr) // процедура печати массива для работы будет принимать в себя массив вещественных чисел,при этом именование 
+{                           // принято массива не имеет связи с именами того же массива в других методах.
     Console.ForegroundColor = ConsoleColor.DarkGreen;
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
@@ -37,12 +39,13 @@ double findMaxMinDiff(double[] arr)
         if (arr[i] < min)
             min = arr[i];
     }
-    return max - min;
+    //double diff = max - min;
+    //return diff;               
+   return max - min; // return передаёт значение, это можно делать без использования переменной.Две строчки выше-более явная логика.
 }
 
-
-double[] array = createRandDoubleArr(10, 5, 15);
-printArr(array);
-double diff = findMaxMinDiff(array);
+double[] array = createRandDoubleArr(10, 5, 15); // array - глобальная переменная которая сначала получает значения из метода 
+printArr(array); // глобальная переменная передаётся в процедуру(void) для вывода массива созданного в методе
+double diff = findMaxMinDiff(array); // метод которому параметром передаём глобальную переменную типа массив выдаёт результат типа double 
 System.Console.Write("Разница между максимальным и минимальным вещественным числом массива--> ");
 Console.WriteLine(Math.Round(diff, 2));
